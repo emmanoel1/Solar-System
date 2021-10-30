6b2d2e298bc624b5a1090b6c833d3200c3e27019
emmanoel <emmanoelporto@gmail.com>
2021-10-30 19:39:07 -0300

    Remove all Coments in Missions.jsx

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -7,17 +7,3 @@ class Missions extends React.Component {
 }
 
 export default Missions;
-
-// Crie um componente chamado Missions dentro da pasta src/components.
-
-// Este componente deve ter uma div que envolva todo seu conteúdo e que tenha o atributo data-testid="missions".
-
-// Renderize o componente Missions abaixo do SolarSystem, dentro do componente principal App.
-
-// O que será verificado:
-
-// Será validado se o componente <Missions /> é renderizado.
-
-// Será validado se existe uma div que possui o data-testid="missions".
-
-// Será validado se o componente <Missions /> está sendo renderizado no componente principal App.
